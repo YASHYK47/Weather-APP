390b61179726ebf8b51199f5e0e3ac653bc1e9e7
Yash Kesarwani <36482238+YASHYK47@users.noreply.github.com>
2018-06-23 21:00:10 +0530

    Update app.js

--- Weather/app.js
+++ Weather/app.js
@@ -1,7 +1,7 @@
 //NOTE-ALL THREE FILES MUST BE IN THE SAME FOLDER WHILE RUNNING THIS APP.JS FILE
 const yargs=require('yargs');
-const geocode=require('./geocode.js');
-const weather=require('./weather.js');
+const geocode=require('./geocode.js');    //To get the latitude and longitude of given address
+const weather=require('./weather.js');    //To get Temperature using that latotude and longitude
 
 var argv=yargs.options({
   a:{
