c238ccaa92056bfc5db1e4778e41664988330f6e
Yash Kesarwani <36482238+YASHYK47@users.noreply.github.com>
2018-06-23 20:50:01 +0530

    Update weather.js

--- Weather/weather.js
+++ Weather/weather.js
@@ -2,7 +2,7 @@ const request=require('request');
 
 module.exports.getWeather=function(lat,lng,callback){
   request({
-    url:`https://api.darksky.net/forecast/da4042315201081260c2b3203bbf6255/${lat},${lng}`,
+    url:`https://api.darksky.net/forecast/da4042315201081260c2b3203bbf6255/${lat},${lng}`,//This is API key is valid for one day only
     json:true
   },(error,response,body)=>{
      if(!error && response.statusCode===200){
